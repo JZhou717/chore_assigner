540425358e4ec9ce25fe748e19f394bd3f756e94
Jake Zhou <xz346@scarletmail.rutgers.edu>
2019-09-06 19:30:00 -0400

    Added ShutDownHook to run serialization

--- src/chore_assigner/Chore_Assigner.java
+++ src/chore_assigner/Chore_Assigner.java
@@ -12,6 +12,11 @@
  * 1. View current data, i.e., people and their amounts per chore
  * 2. Compute future weeks of chores
  * 3. Edit people data 
+ * 
+ * NEED TO IMPLEMENT
+ * ~ Serialization
+ * ~ IO messages
+ * ~ Assignment algorithm
  */
 
 
@@ -19,12 +24,41 @@
 
 package chore_assigner;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Chore_Assigner{
+	
+	/**
+	 * A useful enum when referrring to a specific chore
+	 */
+	public enum Chore {
+		BATHROOM, BREAK, KITCHEN, FLOOR
+	}
+
+	/**
+	 * A list of the housemates. Should be NULL if this program has never been run, otherwise, list should have been read from serialized data
+	 */
+	private ArrayList<Person> people_list;
 	
 	/**
 	 * Main runner method that reads input from user, i.e., me, and outputs computation
 	 */
 	public static void main(String[] args) {
+		//Listener for application termination. Runs serialization on shut down
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				try {
+					serialize();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	private static void serialize() throws IOException {
 		
 	}
 	
--- src/chore_assigner/Person.java
+++ src/chore_assigner/Person.java
@@ -1,13 +1,26 @@
+/**
+ * @author Jake Zhou
+ * There will be a person class for each of the house members. Each Person obj will have a name, a count for each of the 4 jobs, and the most recent chore assigned
+ */
+
 package chore_assigner;
 
 import java.io.Serializable;
+import chore_assigner.Chore_Assigner.Chore;
 
-public abstract class Person implements Serializable {
+public class Person implements Serializable {
 
 	/**
 	 * Default value... no current expectations for expansion or alteration after final version to this class
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	String name;
+	int bathroom_count;
+	int break_count;
+	int kitchen_count;
+	int floors_count;
+	Chore most_recent;
 	
 	
 }
