293ec41b9fedba168370aedfdf557d978d8a67ae
Jake Zhou <xz346@scarletmail.rutgers.edu>
2019-09-06 16:50:41 -0400

    initial push with project foundation

---
+++ src/chore_assigner/Chore_Assigner.java
@@ -0,0 +1,38 @@
+/**
+ * @author Jake Zhou
+ * 
+ * A simple application that assigns chores to my housemates and me for the year. It follows some simple rules and tracks how many times each person has done one of the 4 categories of chores. The data is then serialized so that I do not have to input data everytime I want to make an adjustment to chore assignments.
+ */
+
+
+
+
+/*
+ * WILL have 3 features
+ * 1. View current data, i.e., people and their amounts per chore
+ * 2. Compute future weeks of chores
+ * 3. Edit people data 
+ */
+
+
+
+
+package chore_assigner;
+
+import java.io.Serializable;
+
+public class Chore_Assigner implements Serializable {
+
+	/**
+	 * Default value... no current expectations for expansion or alteration after final version to this class
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Main runner method that reads input from user, i.e., me, and outputs computation
+	 */
+	public static void main(String[] args) {
+		
+	}
+	
+}
